ca4622fb303abb56d4448b28fcb7eeea50e35995
bob_loga <newkey87@gmail.com>
2017-04-02 08:49:36 +0800

    add some text

---
+++ rossi-game1.py
@@ -0,0 +1,13 @@
+b=6645
+a=input("enter your guess: ")
+
+while a!=b:
+    
+    if a == b:
+        print "you guessed correctly"
+    elif a - b <= 20 or a - b <= -20:
+        print "HOTTTT game"
+        break
+    elif a - b <= 50 and a - b > 20 or a - b <= -50 and a - b > -20 :
+        print "Hot but not so hot"
+        break
